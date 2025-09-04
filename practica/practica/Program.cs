aa98cd04829731a5cb5ffe9d891b319e0e87fc2f
Alexio <leandro0870abril@gmail.com>
2025-09-04 00:42:52 -0500

    Hice un cambio

---
+++ practica/practica/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            Console.WriteLine("Ingrese un numero entero");
+            numero = int.Parse(Console.ReadLine());
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("El numero es PAR.");
+
+            }
+            else
+            {
+                Console.WriteLine("El numero es IMPAR");
+
+            }
+
+        }
+    }
+}
